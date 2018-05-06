7681f9fb31c6a2513728c5836c2a6ae9d2163491
BenCharbonneau <benscharb@gmail.com>
2018-05-06 12:56:33 -0500

    set up views/rendered a template

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -1,10 +1,14 @@
 class ApplicationController < Sinatra::Base
 
+	set :root, File.join(File.dirname(__FILE__),'..')
+
+	set :views, Proc.new { File.join(root, "views") }
+
 	require 'bundler'
 	Bundler.require()
 
 	get '/' do
-		"Welcome to the site."
+		erb :basic
 	end
 
 end
